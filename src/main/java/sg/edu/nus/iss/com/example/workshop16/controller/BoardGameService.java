bc9791c4ab6554048a64f1cb34bf899e1bd1d5da
Bryan Chua <Bryanchua.1994@gmail.com>
2023-05-31 14:20:15 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/com/example/workshop16/controller/BoardGameService.java
@@ -0,0 +1,19 @@
+package sg.edu.nus.iss.com.example.workshop16.controller;
+
+import sg.edu.nus.iss.com.example.workshop16.model.Mastermind;
+
+public class BoardGameService {
+
+    public int saveGame(Mastermind ms) {
+        return 0;
+    }
+
+    public Mastermind findById(String msId) {
+        return null;
+    }
+
+    public int updateBoardGame(Mastermind ms) {
+        return 0;
+    }
+
+}
---
+++ src/main/java/sg/edu/nus/iss/com/example/workshop16/repository/Mastermind.java
@@ -0,0 +1,5 @@
+package sg.edu.nus.iss.com.example.workshop16.repository;
+
+public class Mastermind {
+
+}
